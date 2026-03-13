de1704c9166edcf535a7b8b58b3529e5ca346e06
Zangestsru <tsmdoubleliff@gmail.com>
2026-03-13 22:06:01 +0700

    full

--- src/main/java/com/example/ngogiaphuong2280602523/Ngogiaphuong2280602523Application.java
+++ src/main/java/com/example/ngogiaphuong2280602523/Ngogiaphuong2280602523Application.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 		Ngogiaphuong2280602523Application {
+public class Ngogiaphuong2280602523Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ngogiaphuong2280602523Application.class, args);
 	}
 
 }
-
